6a01f1817ca99c19adbea23cce51f29a334cc0f8
Aziz Ibragimov <ozezlus@mail.ru>
2020-07-13 21:31:52 +0200

    Updates

---
+++ precache-manifest.d97f7bca6bdcd6a04a545eb8812912ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82dc90390ca6987a0167d68661a37f2d",
+    "url": "/react-games-shop/index.html"
+  },
+  {
+    "revision": "a10bfea8e4245d84ccb7",
+    "url": "/react-games-shop/static/css/main.d0fef970.chunk.css"
+  },
+  {
+    "revision": "41cdec6d094a5511fc98",
+    "url": "/react-games-shop/static/js/2.5b106aa2.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-games-shop/static/js/2.5b106aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10bfea8e4245d84ccb7",
+    "url": "/react-games-shop/static/js/main.d0d918d6.chunk.js"
+  },
+  {
+    "revision": "62b8947cc9c1921d9314",
+    "url": "/react-games-shop/static/js/runtime-main.4a31b54d.js"
+  },
+  {
+    "revision": "be07ce6c030b856acb0353f0e457763d",
+    "url": "/react-games-shop/static/media/gta-v.be07ce6c.jpg"
+  },
+  {
+    "revision": "4a885e7981733a910d38553cd8a5126c",
+    "url": "/react-games-shop/static/media/metal-gear-5.4a885e79.png"
+  },
+  {
+    "revision": "69a52df5c05ae75b176205b1cba35aac",
+    "url": "/react-games-shop/static/media/red-dead-redemption-2.69a52df5.jpg"
+  },
+  {
+    "revision": "53617b455d61c02d0e000732fcf1baf6",
+    "url": "/react-games-shop/static/media/resident-evil-4.53617b45.jpeg"
+  },
+  {
+    "revision": "157471a7e4813db0e86aa291e57a4bbb",
+    "url": "/react-games-shop/static/media/skyrim.157471a7.jpg"
+  },
+  {
+    "revision": "761d8ed7a71a51d636238b597d7342aa",
+    "url": "/react-games-shop/static/media/the-legend-of-zelda-breath-of-the-wild.761d8ed7.jpg"
+  }
+]);
